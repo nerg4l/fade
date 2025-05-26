3b702294d65c82f19679ff1de88545354569a66c
László GÖRÖG <contact@nergal.xyz>
2025-05-26 12:03:41 +0200

    feat: rotate trainer sprite

--- main.go
+++ main.go
@@ -2,146 +2,21 @@ package main
 
 import (
 	"context"
-	"embed"
 	"errors"
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
 	"github.com/charmbracelet/wish/bubbletea"
 	"github.com/charmbracelet/wish/logging"
-	"github.com/disintegration/imaging"
+	"github.com/muesli/termenv"
 	flag "github.com/spf13/pflag"
-	"image"
-	"image/color"
-	"image/png"
 	"log"
-	"math"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 )
-
-//go:embed sprite/*
-var content embed.FS
-
-type model struct {
-	sprites []string
-	anim    int
-}
-
-func newModel() (*model, error) {
-	m := model{}
-	m.sprites = make([]string, 4)
-
-	idle, err := openImage("sprite/front_idle.png")
-	if err != nil {
-		return nil, err
-	}
-	m.sprites[0] = imageAsString(idle)
-	m.sprites[2] = m.sprites[0]
-	walk, err := openImage("sprite/front_walk.png")
-	if err != nil {
-		return nil, err
-	}
-	m.sprites[1] = imageAsString(walk)
-	m.sprites[3] = imageAsString(imaging.FlipH(walk))
-
-	return &m, nil
-}
-
-func openImage(name string) (image.Image, error) {
-	f, err := content.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	i, err := png.Decode(f)
-	if err != nil {
-		return nil, err
-	}
-	return i, nil
-}
-
-func imageAsString(dec image.Image) string {
-	var b strings.Builder
-	rec := dec.Bounds()
-	for y := 0; y < rec.Dy(); y += 2 {
-		for x := 0; x < rec.Dx(); x++ {
-			top := colorize(dec.At(x, y))
-			bottom := colorize(dec.At(x, y+1))
-			if Luminance(top) > Luminance(bottom) {
-				b.WriteString(lipgloss.NewStyle().
-					Foreground(top).
-					Background(bottom).
-					Render("▀"))
-			} else if Luminance(top) < Luminance(bottom) {
-				b.WriteString(lipgloss.NewStyle().
-					Foreground(bottom).
-					Background(top).
-					Render("▄"))
-			} else if Luminance(top) < Luminance(color.RGBA{R: 128, G: 128, B: 128, A: 255}) {
-				b.WriteString(lipgloss.NewStyle().
-					Foreground(top).
-					Background(lipgloss.Color("#ffffff")).
-					Render("█"))
-			} else {
-				b.WriteString(lipgloss.NewStyle().
-					Background(bottom).
-					Render(" "))
-			}
-		}
-		b.WriteString("\n")
-	}
-	return b.String()
-}
-
-func Luminance(c color.Color) float64 {
-	r, g, b, _ := c.RGBA()
-	return 0.2126*toSRGB(uint8(r)) + 0.7152*toSRGB(uint8(g)) + 0.0722*toSRGB(uint8(b))
-}
-
-func toSRGB(i uint8) float64 {
-	v := float64(i) / 255
-	if v <= 0.04045 {
-		return v / 12.92
-	} else {
-		return math.Pow((v+0.055)/1.055, 2.4)
-	}
-}
-
-func (m model) Init() tea.Cmd {
-	return tea.HideCursor
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "down":
-			m.anim++
-			m.anim %= len(m.sprites)
-			return m, nil
-		}
-	}
-	return m, nil
-}
-
-func (m model) View() string {
-	return m.sprites[m.anim]
-}
-
-func colorize(color color.Color) lipgloss.Color {
-	r, g, b, a := color.RGBA()
-	if a == 0 {
-		return lipgloss.Color("#ffffff")
-	}
-	return lipgloss.Color(fmt.Sprintf("#%02x%02x%02x", r, g, b))
-}
 
 func main() {
 	addr := flag.StringP("addr", "a", "0.0.0.0:5000", "SSH server port")
@@ -173,9 +48,10 @@ func main() {
 		// Do not accept password auth.
 		ssh.PasswordAuth(func(ssh.Context, string) bool { return false }),
 		wish.WithMiddleware(
-			bubbletea.Middleware(func(sess ssh.Session) (tea.Model, []tea.ProgramOption) {
+			bubbletea.MiddlewareWithColorProfile(func(sess ssh.Session) (tea.Model, []tea.ProgramOption) {
+				m.r = bubbletea.MakeRenderer(sess)
 				return m, append(options, tea.WithContext(sess.Context()))
-			}),
+			}, termenv.ANSI),
 			logging.Middleware(),
 		),
 	)
---
+++ sprite.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/disintegration/imaging"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"strings"
+)
+
+//go:embed sprite/*
+var content embed.FS
+
+type spriteTrainer struct {
+	sprites map[string][]image.Image
+	face    string
+	anim    int
+
+	r *lipgloss.Renderer
+}
+
+func newModel() (*spriteTrainer, error) {
+	m := spriteTrainer{face: "down"}
+	m.r = lipgloss.DefaultRenderer()
+	m.sprites = map[string][]image.Image{
+		"down":  make([]image.Image, 4),
+		"up":    make([]image.Image, 4),
+		"left":  make([]image.Image, 2),
+		"right": make([]image.Image, 2),
+	}
+
+	idle, err := openImage("sprite/front_idle.png")
+	if err != nil {
+		return nil, err
+	}
+	walk, err := openImage("sprite/front_walk.png")
+	if err != nil {
+		return nil, err
+	}
+	m.sprites["down"][0] = idle
+	m.sprites["down"][1] = walk
+	m.sprites["down"][2] = m.sprites["down"][0]
+	m.sprites["down"][3] = imaging.FlipH(walk)
+
+	idle, err = openImage("sprite/back_idle.png")
+	if err != nil {
+		return nil, err
+	}
+	walk, err = openImage("sprite/back_walk.png")
+	if err != nil {
+		return nil, err
+	}
+	m.sprites["up"][0] = idle
+	m.sprites["up"][1] = walk
+	m.sprites["up"][2] = m.sprites["up"][0]
+	m.sprites["up"][3] = imaging.FlipH(walk)
+
+	idle, err = openImage("sprite/side_idle.png")
+	if err != nil {
+		return nil, err
+	}
+	walk, err = openImage("sprite/side_walk.png")
+	if err != nil {
+		return nil, err
+	}
+	m.sprites["left"][0] = idle
+	m.sprites["left"][1] = walk
+	m.sprites["right"][0] = imaging.FlipH(idle)
+	m.sprites["right"][1] = imaging.FlipH(walk)
+
+	return &m, nil
+}
+
+func openImage(name string) (image.Image, error) {
+	f, err := content.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	i, err := png.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	return i, nil
+}
+
+var (
+	PalletWhite     = "#f8f8f8"
+	PalletBlack     = "#141414"
+	PalletHighlight = "#a8a8a8"
+)
+
+func imageAsString(r *lipgloss.Renderer, dec image.Image) string {
+	var b strings.Builder
+	rec := dec.Bounds()
+	for y := 0; y < rec.Dy(); y += 2 {
+		for x := 0; x < rec.Dx(); x++ {
+			top := dec.At(x, y)
+			bottom := dec.At(x, y+1)
+			if Luminance(top) < Luminance(bottom) {
+				b.WriteString(r.NewStyle().
+					Foreground(colorize(top)).
+					Background(colorize(bottom)).
+					Render("▀"))
+			} else if Luminance(top) > Luminance(bottom) {
+				b.WriteString(r.NewStyle().
+					Foreground(colorize(bottom)).
+					Background(colorize(top)).
+					Render("▄"))
+			} else if Luminance(top) < Luminance(color.RGBA{R: 128, G: 128, B: 128, A: 255}) {
+				b.WriteString(r.NewStyle().
+					Foreground(colorize(top)).
+					Background(lipgloss.Color("#ffffff")).
+					Render("█"))
+			} else {
+				b.WriteString(r.NewStyle().
+					Background(colorize(bottom)).
+					Render(" "))
+			}
+		}
+		b.WriteString("\n")
+	}
+	return b.String()
+}
+
+func Luminance(c color.Color) float64 {
+	r, g, b, _ := c.RGBA()
+	return 0.2126*toSRGB(uint8(r)) + 0.7152*toSRGB(uint8(g)) + 0.0722*toSRGB(uint8(b))
+}
+
+func toSRGB(i uint8) float64 {
+	v := float64(i) / 255
+	if v <= 0.04045 {
+		return v / 12.92
+	} else {
+		return math.Pow((v+0.055)/1.055, 2.4)
+	}
+}
+
+func (m spriteTrainer) Init() tea.Cmd {
+	return tea.HideCursor
+}
+
+func (m spriteTrainer) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "down":
+			m.face = "down"
+			m.anim++
+		case "up":
+			m.face = "up"
+			m.anim++
+		case "left":
+			m.face = "left"
+			m.anim++
+		case "right":
+			m.face = "right"
+			m.anim++
+		}
+	}
+	m.anim %= len(m.sprites[m.face])
+	return m, nil
+}
+
+func (m spriteTrainer) View() string {
+	return imageAsString(m.r, m.sprites[m.face][m.anim])
+}
+
+func colorize(color color.Color) lipgloss.TerminalColor {
+	r, g, b, a := color.RGBA()
+	if a == 0 {
+		return lipgloss.CompleteColor{TrueColor: PalletWhite, ANSI256: "250", ANSI: "7"}
+	}
+	c := fmt.Sprintf("#%02x%02x%02x", uint8(r), uint8(g), uint8(b))
+	switch c {
+	case PalletWhite:
+		return lipgloss.CompleteColor{TrueColor: PalletWhite, ANSI256: "250", ANSI: "7"}
+	case PalletBlack:
+		return lipgloss.CompleteColor{TrueColor: PalletBlack, ANSI256: "237", ANSI: "0"}
+	case PalletHighlight:
+		return lipgloss.CompleteColor{TrueColor: "#cc0000", ANSI256: "124", ANSI: "1"}
+	default:
+		return lipgloss.Color("3")
+	}
+}
